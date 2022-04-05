da48993d78bcbf85dc9eee191809dd179f8d978c
Filipe Soares <filipeseventeen1@gmail.com>
2022-04-05 10:07:37 -0300

    Criado a tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,68 @@
-import React from 'react'
-import { Container } from './style'
+import React, { useState } from 'react'
+import {
+  Container,
+  Banner,
+  Wrapper,
+  Title,
+  InputItem,
+  InputIcon,
+  Input,
+  InputIconPassword,
+  ForgotPassword,
+  ForgotPasswordText,
+  SubmitButton,
+  SubmitButtonText,
+  RegisterButton,
+  RegisterButtonText,
+  ScrollableContainer
+} from './style'
+
+import { FontAwesome } from '@expo/vector-icons'
 
 export const Login: React.FC = () => {
-  return <Container></Container>
+  const [passwordIsHide, setPasswordIsHide] = useState(true)
+
+  const toggleHidePassword = () => setPasswordIsHide(!passwordIsHide)
+  return (
+    <Container>
+      <ScrollableContainer contentContainerStyle={{ flexGrow: 1 }}>
+        <Banner source={require('../../../assets/login/banner.png')} />
+        <Wrapper>
+          <Title>Faça seu Login</Title>
+          <InputItem style={{ elevation: 10 }}>
+            <InputIcon>
+              <FontAwesome name="user" size={28} color="rgba(77, 86, 109, 0.46)" />
+            </InputIcon>
+            <Input placeholder="E-mail" />
+          </InputItem>
+          <InputItem style={{ elevation: 10 }}>
+            <InputIcon>
+              <FontAwesome name="key" size={28} color="rgba(77, 86, 109, 0.46)" />
+            </InputIcon>
+            <Input placeholder="Senha" secureTextEntry={passwordIsHide} />
+            <InputIconPassword onPress={toggleHidePassword}>
+              {passwordIsHide ? (
+                <FontAwesome name="eye" size={28} color="rgba(77, 86, 109, 0.46)" />
+              ) : (
+                <FontAwesome
+                  name="eye-slash"
+                  size={28}
+                  color="rgba(77, 86, 109, 0.46)"
+                />
+              )}
+            </InputIconPassword>
+          </InputItem>
+          <ForgotPassword>
+            <ForgotPasswordText>Esqueceu sua senha?</ForgotPasswordText>
+          </ForgotPassword>
+          <SubmitButton>
+            <SubmitButtonText>Logar</SubmitButtonText>
+          </SubmitButton>
+          <RegisterButton>
+            <RegisterButtonText>Cadastre-se</RegisterButtonText>
+          </RegisterButton>
+        </Wrapper>
+      </ScrollableContainer>
+    </Container>
+  )
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,3 +1,94 @@
 import styled from 'styled-components/native'
+import { primary } from '../../styles/globalCssVar'
 
-export const Container = styled.View``
+export const ScrollableContainer = styled.ScrollView``
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Banner = styled.Image`
+  height: 340px;
+  width: 100%;
+`
+
+export const Wrapper = styled.View`
+  background: #fff;
+  flex: 1;
+  margin-top: -50px;
+
+  border-top-left-radius: 72px;
+  border-top-right-radius: 72px;
+
+  align-items: center;
+`
+
+export const Title = styled.Text`
+  color: ${primary};
+  font-size: 22px;
+  font-weight: 700;
+  text-align: center;
+  margin: 22px 0;
+`
+
+export const InputItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: 70px;
+  width: 80%;
+  padding: 0 12px;
+  margin-bottom: 16px;
+  border-radius: 6px;
+  background: #fff;
+`
+
+export const InputIcon = styled.View`
+  width: 10%;
+  justify-content: center;
+  align-items: center;
+`
+
+export const InputIconPassword = styled.TouchableWithoutFeedback`
+  width: 10%;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Input = styled.TextInput`
+  width: 80%;
+  height: 100%;
+  font-size: 18px;
+  padding-left: 12px;
+`
+
+export const ForgotPassword = styled.TouchableOpacity``
+
+export const ForgotPasswordText = styled.Text`
+  text-decoration: underline;
+  color: rgba(77, 86, 109, 0.46);
+  font-size: 18px;
+`
+
+export const SubmitButton = styled.TouchableOpacity`
+  background: ${primary};
+  width: 80%;
+  height: 60px;
+  border-radius: 6px;
+  margin-top: 22px;
+  justify-content: center;
+  align-items: center;
+`
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+`
+
+export const RegisterButton = styled.TouchableOpacity``
+
+export const RegisterButtonText = styled.Text`
+  color: #797f8e;
+  font-size: 20px;
+  margin-top: 16px;
+`
