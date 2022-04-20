15cf0d56bb108386f54f77a8c677634455cd22ba
Filipe Soares <filipeseventeen1@gmail.com>
2022-04-20 10:32:59 -0300

    Feito a tela de planos

--- src/pages/Plans/index.tsx
+++ src/pages/Plans/index.tsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text } from 'react-native'
-import { Container } from './style'
+import {
+  BackButton,
+  Banner,
+  Button,
+  ButtonText,
+  Container,
+  Description,
+  Header,
+  Notification,
+  Plan,
+  PlanArea,
+  PlanNumber,
+  PlanText,
+  Title,
+  Wrapper
+} from './style'
+
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
 export const Plans: React.FC = () => {
+  const [threeMonthIsActive, setThreeMonthIsActive] = useState(false)
+  const [sixMonthIsActive, setSixMonthIsActive] = useState(true)
+  const [twelveMonthIsActive, setTwelveMonthIsActive] = useState(false)
+
+  const activeThreeMonth = () => {
+    setThreeMonthIsActive(true)
+    setSixMonthIsActive(false)
+    setTwelveMonthIsActive(false)
+  }
+
+  const activeSixMonth = () => {
+    setThreeMonthIsActive(false)
+    setSixMonthIsActive(true)
+    setTwelveMonthIsActive(false)
+  }
+
+  const activeTwelveMonth = () => {
+    setThreeMonthIsActive(false)
+    setSixMonthIsActive(false)
+    setTwelveMonthIsActive(true)
+  }
+
   return (
     <Container>
-      <Text>Planos</Text>
+      <Header>
+        <BackButton>
+          <FontAwesome name="arrow-left" size={24} color="#777d8c" />
+        </BackButton>
+        <Notification>
+          <Ionicons name="notifications-outline" size={22} color="#777d8c" />
+        </Notification>
+      </Header>
+      <Wrapper>
+        <Banner
+          resizeMode="contain"
+          source={require('../../../assets/plans/banner.png')}
+        />
+        <Title>Contrate um plano para poder publicar</Title>
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+          bibendum laoreet. Proin gravida dolor sit amet lacus
+        </Description>
+        <PlanArea>
+          <Plan isActive={threeMonthIsActive} onPress={activeThreeMonth}>
+            <PlanNumber isActive={threeMonthIsActive}>3</PlanNumber>
+            <PlanText isActive={threeMonthIsActive}>meses</PlanText>
+          </Plan>
+          <Plan isActive={sixMonthIsActive} onPress={activeSixMonth}>
+            <PlanNumber isActive={sixMonthIsActive}>6</PlanNumber>
+            <PlanText isActive={sixMonthIsActive}>meses</PlanText>
+          </Plan>
+          <Plan isActive={twelveMonthIsActive} onPress={activeTwelveMonth}>
+            <PlanNumber isActive={twelveMonthIsActive}>12</PlanNumber>
+            <PlanText isActive={twelveMonthIsActive}>meses</PlanText>
+          </Plan>
+        </PlanArea>
+      </Wrapper>
+      <Button>
+        <ButtonText>Contratar</ButtonText>
+      </Button>
     </Container>
   )
 }
--- src/pages/Plans/style.ts
+++ src/pages/Plans/style.ts
@@ -1,3 +1,93 @@
 import styled from 'styled-components/native'
+import { primary, secondary } from '../../styles/globalCssVar'
 
-export const Container = styled.View``
+export const Container = styled.ScrollView`
+  flex: 1;
+`
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 22px;
+`
+
+export const BackButton = styled.TouchableOpacity`
+  z-index: 2;
+`
+
+export const Notification = styled.TouchableOpacity`
+  z-index: 2;
+`
+
+export const Wrapper = styled.View`
+  padding: 0 22px;
+`
+
+export const Banner = styled.Image`
+  width: 110%;
+  height: 400px;
+  align-self: center;
+  margin-top: -62px;
+`
+
+export const Title = styled.Text`
+  color: ${primary};
+  font-size: 26px;
+  text-align: center;
+  width: 250px;
+  align-self: center;
+  margin-top: -30px;
+  margin-bottom: 12px;
+`
+
+export const Description = styled.Text`
+  text-align: center;
+  color: rgba(5, 5, 6, 0.59);
+  font-size: 18px;
+`
+
+export const PlanArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 22px;
+`
+
+interface PlanProps {
+  isActive: boolean
+}
+
+export const Plan = styled.TouchableOpacity<PlanProps>`
+  background: ${(props) => (props.isActive ? primary : '#fff')};
+  width: 109px;
+  height: 106px;
+  padding: 2px;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const PlanNumber = styled.Text<PlanProps>`
+  color: ${(props) => (props.isActive ? '#fff' : primary)};
+  font-weight: bold;
+  font-size: 34px;
+`
+
+export const PlanText = styled.Text<PlanProps>`
+  color: ${(props) => (props.isActive ? '#fff' : primary)};
+  font-size: 20px;
+`
+
+export const Button = styled.TouchableOpacity`
+  background: ${secondary};
+  justify-content: center;
+  align-items: center;
+  padding: 2px;
+  height: 65px;
+  margin: 32px 0;
+`
+
+export const ButtonText = styled.Text`
+  color: #fff;
+`
